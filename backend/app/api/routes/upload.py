42b391888e5597ea0a348c4e7be66679ab9256b0
kidclone3 <kid.clone.3@gmail.com>
2024-06-21 18:30:17 +0700

    feat: add function to render html files

--- backend/app/api/routes/upload.py
+++ backend/app/api/routes/upload.py
@@ -1,5 +1,4 @@
 import os
-import shutil
 import uuid
 from typing import Any
 
@@ -14,8 +13,18 @@ from app.models import MarkdownFile, HTMLFile
 router = APIRouter()
 
 
+def create_output_file(upload_dir: str, output_dir: str, filename: str):
+    # TODO: remove later
+    deploy_dir = "/home/delus/Documents/code/personal/pandoc-cv/deploy"
+    os.system(f"cp {upload_dir}/{filename}.md {deploy_dir}/{filename}.md")
+    os.system(f"bash {deploy_dir}/run_md2html.sh {deploy_dir} {filename}")
+    os.system(f"cp {deploy_dir}/{filename}.html {output_dir}/{filename}.html")
+    os.system(f"bash {deploy_dir}/cleanup_file.sh {deploy_dir} {filename}")
+
+
 @router.post("/uploadfile/")
-async def create_upload_file(file: UploadFile = File(...), session: Session = Depends(get_session)):
+async def create_upload_file(file: UploadFile = File(...), session: Session = Depends(get_session)
+                             ):
     # TODO: change all logic to services
     uploaded_file = file
     print(file)
@@ -23,7 +32,7 @@ async def create_upload_file(file: UploadFile = File(...), session: Session = De
     if not os.path.exists(upload_dir):
         os.makedirs(upload_dir)
     new_uid = str(uuid.uuid4())
-    file_path = os.path.join(upload_dir, new_uid)
+    file_path = os.path.join(upload_dir, f"{new_uid}.md")
     with open(file_path, "wb") as f:
         f.write(await uploaded_file.read())
 
@@ -35,14 +44,11 @@ async def create_upload_file(file: UploadFile = File(...), session: Session = De
     output_dir = settings.DATA_OUTPUT_FOLDER_PATH
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
-    # Create output file
-    # TODO: Change logic later.
-    output_file_path = os.path.join(output_dir, new_uid + ".html")
-    #  Copy file from parent folder of output_dir to this
-    parent_dir = os.path.dirname(output_dir)
-    file_to_copy = os.path.join(parent_dir, "test.html")
-    shutil.copyfile(file_to_copy, output_file_path)
 
+    output_file_path = os.path.join(output_dir, new_uid + ".html")
+    # task = BackgroundTasks()
+    # task.add_task(create_output_file, upload_dir, output_dir, new_uid)
+    create_output_file(upload_dir, output_dir, new_uid)
     try:
         session.add(file_instance)
         session.flush()
