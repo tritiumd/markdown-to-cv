2821d520c0ac35a35a52980c423d1b42ca96c11a
kidclone3 <kid.clone.3@gmail.com>
2024-07-04 10:41:17 +0700

    hot fix padding main page

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -4,12 +4,12 @@ import UploadField from "@/components/upload";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-2 break-words">
+    <main className="flex min-h-screen flex-col items-center justify-between break-words">
       <MainNav className="flex" />
 
-      <UploadField className="flex-row grow w-full h-4/5"/>
+      <UploadField className="flex-row grow w-full h-4/5" />
 
-      <Footer className="mt-auto"/>
+      <Footer className="mt-auto" />
     </main>
   );
 }
