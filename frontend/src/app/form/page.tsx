b50afe2280f84b579d01abc5a1309bc46d144340
kidclone3 <kid.clone.3@gmail.com>
2024-07-04 14:46:58 +0700

    update: i think it might look like hackmd

--- frontend/src/app/form/page.tsx
+++ frontend/src/app/form/page.tsx
@@ -13,22 +13,22 @@ export default function FormPage() {
   return (
     <main className="flex min-h-screen max-w-full flex-col items-center justify-between break-words">
       <MainNav className="" />
-      <ResizablePanelGroup direction="horizontal" className="overflow-auto">
-        <ResizablePanel defaultSize={34} minSize={30} order={1}>
+      <ResizablePanelGroup direction="horizontal">
+        <ResizablePanel defaultSize={50} minSize={30} order={1}>
           <CvForm />
         </ResizablePanel>
         <ResizableHandle className="" />
 
-        <ResizablePanel defaultSize={67} order={2}>
+        <ResizablePanel defaultSize={50} order={2}>
           <Image
             src="/assets/Image_placeholder_upright.png"
             alt="hero"
-            width={700}
-            height={700}
+            width={500}
+            height={500}
           />
         </ResizablePanel>
       </ResizablePanelGroup>
-      <Footer className="h-32" />
+      {/* <Footer className="h-32" /> */}
     </main>
   );
 }
