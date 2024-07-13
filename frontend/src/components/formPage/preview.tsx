dfa21334769e52cc5d3b93ee759672b50c1c1f0b
kidclone3 <kid.clone.3@gmail.com>
2024-07-13 20:02:54 +0700

    feat: :art: add loading button

--- frontend/src/components/formPage/preview.tsx
+++ frontend/src/components/formPage/preview.tsx
@@ -7,10 +7,13 @@ import parse from "node-html-parser";
 import { useSelector } from "react-redux";
 import { getApiUrl } from "@/store/slice";
 import { Button } from "../ui/button";
+import { Loader2 } from "lucide-react";
 const Preview: React.FC = () => {
   const [content, setContent] = React.useState<string>("");
   const currentUrl = useSelector(getApiUrl);
+  const [isLoading, setIsLoading] = React.useState<boolean>(true);
   React.useEffect(() => {
+    setIsLoading(true);
     const fetchData = () => {
       fetch(currentUrl)
         .then((res) => {
@@ -26,36 +29,27 @@ const Preview: React.FC = () => {
         .then((data) => {
           const root = parse.parse(data);
           setContent(root.toString());
+          setIsLoading(false);
+          console.log(content);
         })
         .catch((error) => console.error("Failed to fetch data:", error));
     };
 
-    const timer = setTimeout(() => {
-      fetchData(); // Initial fetch
-    }, 2000); // 2000 milliseconds delay
-
-    return () => clearTimeout(timer); // Cleanup the timeout on component unmount or when currentUrl changes
-  }, [currentUrl]);
+    fetchData(); // Initial fetch
+  }, [currentUrl, content]);
   return (
     <div className="w-full h-full">
       <Card className="m-auto w-full h-full">
-        {/* Reload button */}
-        <CardContent className="flex justify-end">
-          <Button
-            onClick={() => {
-              fetch(currentUrl)
-                .then((res) => res.text())
-                .then((data) => {
-                  const root = parse.parse(data);
-                  setContent(root.toString());
-                })
-                .catch((error) =>
-                  console.error("Failed to fetch data:", error)
-                );
-            }}
-          />
-        </CardContent>
-        <iframe className="w-full h-full" srcDoc={content}></iframe>
+        {isLoading ? (
+          <div className="flex justify-center items-center w-full h-full">
+            <Button disabled>
+              <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+              Please wait
+            </Button>
+          </div>
+        ) : (
+          <iframe className="w-full h-full" srcDoc={content}></iframe>
+        )}
       </Card>
     </div>
   );
