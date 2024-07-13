92045890ab5324b40e063de0d118a889c017a775
kidclone3 <kid.clone.3@gmail.com>
2024-07-13 12:11:55 +0700

    fix: add timer

--- frontend/src/components/formPage/preview.tsx
+++ frontend/src/components/formPage/preview.tsx
@@ -10,13 +10,17 @@ const Preview: React.FC = () => {
   const [content, setContent] = React.useState<string>("");
   const currentUrl = useSelector(getApiUrl);
   React.useEffect(() => {
-    fetch(currentUrl)
-      .then((res) => res.text())
-      .then((data) => {
-        const root = parse.parse(data);
-        setContent(root.toString());
-      })
-      .catch((error) => console.error("Failed to fetch data:", error));
+    const timer = setTimeout(() => {
+      fetch(currentUrl)
+        .then((res) => res.text())
+        .then((data) => {
+          const root = parse.parse(data);
+          setContent(root.toString());
+        })
+        .catch((error) => console.error("Failed to fetch data:", error));
+    }, 2000); // 2000 milliseconds delay
+
+    return () => clearTimeout(timer); // Cleanup the timeout on component unmount or when currentUrl changes
   }, [currentUrl]);
   return (
     <div className="w-full h-full">
